dac55c634b8451a3b25006612e4d9613b65a3ce9
ChaosIsFramecode <framecodechaos@gmail.com>
2024-02-03 21:56:44 +0100

    Formatting

--- cmd/slipice/main.go
+++ cmd/slipice/main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "log"
-  "os"
-  "os/signal"
-  "syscall"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
-  "github.com/bwmarrin/discordgo"
-  "github.com/joho/godotenv"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 
-  "github.com/arcticstrap/slipice/pkg/textparse"
+	"github.com/arcticstrap/slipice/pkg/textparse"
 )
 
 func main() {
-  // Load environment variables
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatalf("Failed to load .env file: %v", err)
-  }
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
+	}
 
-  BotToken := os.Getenv("BOTTOKEN")
+	BotToken := os.Getenv("BOTTOKEN")
 
-  // Init bot client
-  bClient, err := discordgo.New("Bot " + BotToken)
-  if err != nil {
-    log.Fatalf("Failed to initalize bot client: %v", err)
-  }
+	// Init bot client
+	bClient, err := discordgo.New("Bot " + BotToken)
+	if err != nil {
+		log.Fatalf("Failed to initalize bot client: %v", err)
+	}
 
-  // Register events
-  bClient.AddHandler(textparse.OnMessageCreate)
+	// Register events
+	bClient.AddHandler(textparse.OnMessageCreate)
 
-  // Listen to connection
-  err = bClient.Open()
-  if err != nil {
-    log.Fatalf("Failed to open bot client: %v", err)
-  }
-  defer bClient.Close()
+	// Listen to connection
+	err = bClient.Open()
+	if err != nil {
+		log.Fatalf("Failed to open bot client: %v", err)
+	}
+	defer bClient.Close()
 
-  // Yield until a term signal is called
-  log.Println("Bot running! Press CTRL-C to exit.")
-  sc := make(chan os.Signal,1)
-  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-  <-sc
+	// Yield until a term signal is called
+	log.Println("Bot running! Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
